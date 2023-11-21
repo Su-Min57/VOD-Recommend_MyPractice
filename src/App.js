737485086929875670cc651d3369e6d25db4e895
Su-Min57 <hera2268@naver.com>
2023-11-21 16:56:04 +0900

    첫번째 커밋

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {Routes, Route, Link} from "react-router-dom";
+
+import Home from "./main/Home.js";
+import TVdrama from "./main/TVdrama.js";
+import Counter from "./main/Counter.js";
+import Search from "./main/Search.js";
+import Search2 from "./main/Search2.js";
+
+import { dummy } from './movieDummy';
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav>
+        <Link to="/">홈</Link> | <Link to="/TVdrama">TV드라마</Link> | 
+        <Link to="/Counter">Counter</Link> |
+        <Link to="/search">검색</Link> | 
+        <Link to="/search2">검색2</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/TVdrama" element={<TVdrama />} />
+        <Route path="/Counter" element={<Counter />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/search2" element={<Search2 />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/main/TVdrama.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+const TVdrama = () => {
+    return <h1>드라마 화면 입니다.</h1>;
+}
+
+export default TVdrama;
